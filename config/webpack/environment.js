0893f05e224d83bd5eebf689c43178d49f55c8de
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-12-10 09:52:01 -0500

    Add comment about source of strange resolve-url-loader config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+// Place resolve-url-loader into webpack loaders config
+// source: https://github.com/rails/webpacker/issues/2155#issuecomment-829741240
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 })
